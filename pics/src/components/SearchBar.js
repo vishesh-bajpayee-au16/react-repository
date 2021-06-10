f2589519d6381f32a83199ed12be7d1a03b9f85b
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-06-10 22:08:39 +0530

    added search bar form submit

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "", cal: "abc" };
+
+  onformSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onformSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
